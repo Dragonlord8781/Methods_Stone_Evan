bb4ca3ace03468f9be1ad1ff271dc1e145f0413b
Dragonlord8781 <evan.stone638@gmail.com>
2025-03-14 10:15:56 -0500

    finished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // asks for name
+            Console.WriteLine("Please enter name.");
+            //initalises the string userName with the inputted name
+            string userName = Console.ReadLine();
+            //activates Introduction
+            Introduction(userName);
+            // writes a line give a space inbetween Introduction and Donadogohvi
+            Console.WriteLine("...");
+            // actuvates Donadogohvi
+            Donadogohvi();
+        }
+        //creates introduction with factor of string userName
+        static void Introduction(string userName)
+        {
+            //writes line on activation, with userName as a component, greeting the user
+            Console.WriteLine($"Hello, {userName}! It is nice to meet you!");
+        }
+        //creates Donadogohvi
+        static void Donadogohvi()
+        {
+            //writes line telling the user to have a fantastic day
+            Console.WriteLine("Have a fantastic day!");
+        }
+    }
+}
